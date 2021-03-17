75940bb77ef7fb82fafb5dd739cf073bc469a278
EnzoKG <enzogiovanini@gmail.com>
2021-03-17 16:02:18 -0400

    Configurando app

--- precos_produtos/price/models.py
+++ precos_produtos/price/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pesquisa(models.Model):
+    titulo = models.CharField(max_length=200, default='nulo')
+    preco_parcelado = models.CharField(max_length=50, default='nulo')
+    preco_boleto = models.CharField(max_length=50, default='nulo')
+    data_inserida = models.DateField()
+
+    def __str__(self):
+        return self.titulo
